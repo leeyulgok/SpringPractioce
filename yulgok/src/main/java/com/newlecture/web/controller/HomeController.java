8aaaf7ce91f6e73c8d24b74e4b7fc6d95fcccd2f
leeyulgok <dbfrhr20@gmail.com>
2022-04-27 18:33:30 +0900

    Spring Practice

---
+++ yulgok/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -0,0 +1,27 @@
+ package com.newlecture.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping("index")
+	@ResponseBody
+	public String index() {
+		return "Hello Index";
+	}
+	
+//	@Override
+//	public ModelAndView handleRequest(Http ServletRequest request, HttpServletResponse response) throws Exception {
+//		
+//		ModelAndView mv = new ModelAndView("root.index");
+//		
+//		mv.addObject("data", "Hello Spring MVC~");
+//		//mv.setViewName("/WEB-INF/view/index.jsp");
+//		return mv;
+//	}
+
+}
